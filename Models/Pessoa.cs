5d2dfa4d9f466b010bf5db2a3f8af0138d4ef0ad
Rhemerson <lira_Rhemerson@hotmail.com>
2022-06-12 19:34:38 -0300

    Adicionar arquivos de projeto.

---
+++ Models/Pessoa.cs
@@ -0,0 +1,11 @@
+﻿namespace TesteEngegraph.Models
+{
+    public class Pessoa
+    {
+        public int Id { get; set; }
+        public string CPF { get; set; }
+        public string Nome { get; set; }
+        public DateTime Dt_nascimento { get; set; }
+        public string Sexo { get; set; }
+    }
+}
