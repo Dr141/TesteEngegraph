65319255dae15d735583b6124abeca6e550b60ff
Rhemerson <lira_Rhemerson@hotmail.com>
2022-06-12 23:51:03 -0300

    Criado as class pessoa e tipo_pessoa. Implementado as migration.

---
+++ Migrations/20220613010027_initil.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace TesteEngegraph.Migrations
+{
+    public partial class initil : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Pessoa",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    CPF = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Nome = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Dt_nascimento = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    Sexo = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Pessoa", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Tipo_Pessoa",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Descricao = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    PessoaId = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Tipo_Pessoa", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Tipo_Pessoa_Pessoa_PessoaId",
+                        column: x => x.PessoaId,
+                        principalTable: "Pessoa",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Tipo_Pessoa_PessoaId",
+                table: "Tipo_Pessoa",
+                column: "PessoaId",
+                unique: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Tipo_Pessoa");
+
+            migrationBuilder.DropTable(
+                name: "Pessoa");
+        }
+    }
+}
---
+++ Models/Tipo_Pessoa.cs
@@ -0,0 +1,10 @@
+﻿namespace TesteEngegraph.Models
+{
+    public class Tipo_Pessoa
+    {
+        public int Id { get; set; }
+        public string Descricao { get; set; }
+        public int PessoaId { get; set; }
+        public virtual Pessoa Pessoa { get; set; }
+    }
+}
